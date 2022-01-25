c6d4930f8e820733540858cabb22d75ee3f83c1f
Lucas Romero Magaña <lucasromeromagana@gmail.com>
2022-01-25 10:12:52 +0100

    Initial Commit

---
+++ GestorEmpleados/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GestorEmpleados
+//
+//  Created by Lucas Romero Magaña on 25/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
